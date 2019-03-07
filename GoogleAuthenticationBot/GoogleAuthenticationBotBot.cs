cfdda303bbce3f85c978a02f93ec8a5bf6d8ef21
Hinault <ngdonfack@gmail.com>
2019-03-06 21:37:47 -0500

    refactoring GoogleAuthenticationBot

--- GoogleAuthenticationBot/GoogleAuthenticationBotBot.cs
+++ GoogleAuthenticationBot/GoogleAuthenticationBotBot.cs
@@ -45,8 +45,7 @@ namespace GoogleAuthenticationBot
 
             _accessors = new GoogleAuthenticationBotAccessors(conversationState)
             {
-                CounterState = conversationState.CreateProperty<CounterState>(GoogleAuthenticationBotAccessors.CounterStateName),
-            };
+                          };
 
             _logger = loggerFactory.CreateLogger<GoogleAuthenticationBotBot>();
             _logger.LogTrace("Turn start.");
@@ -67,31 +66,7 @@ namespace GoogleAuthenticationBot
         /// <seealso cref="IMiddleware"/>
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
-            // Handle Message activity type, which is the main activity type for shown within a conversational interface
-            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
-            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
-            if (turnContext.Activity.Type == ActivityTypes.Message)
-            {
-                // Get the conversation state from the turn context.
-                var state = await _accessors.CounterState.GetAsync(turnContext, () => new CounterState());
-
-                // Bump the turn count for this conversation.
-                state.TurnCount++;
-
-                // Set the property using the accessor.
-                await _accessors.CounterState.SetAsync(turnContext, state);
-
-                // Save the new turn count into the conversation state.
-                await _accessors.ConversationState.SaveChangesAsync(turnContext);
-
-                // Echo back to the user whatever they typed.
-                var responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'\n";
-                await turnContext.SendActivityAsync(responseMessage);
-            }
-            else
-            {
-                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
-            }
+          
         }
     }
 }
