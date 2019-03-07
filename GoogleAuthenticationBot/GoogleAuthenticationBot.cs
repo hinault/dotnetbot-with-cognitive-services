66efcc1c0b425a86f80a370cba6911b0b3dc1cf3
Hinault <ngdonfack@gmail.com>
2019-03-06 22:31:52 -0500

    update bot file

--- GoogleAuthenticationBot/GoogleAuthenticationBot.cs
+++ GoogleAuthenticationBot/GoogleAuthenticationBot.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 
@@ -24,31 +26,42 @@ namespace GoogleAuthenticationBot
     {
         private readonly GoogleAuthenticationBotAccessors _accessors;
         private readonly ILogger _logger;
-
+        private DialogSet _dialogs;
         /// <summary>
         /// Initializes a new instance of the class.
         /// </summary>
         /// <param name="conversationState">The managed conversation state.</param>
         /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
         /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
-        public GoogleAuthenticationBot(ConversationState conversationState, ILoggerFactory loggerFactory)
+        public GoogleAuthenticationBot(GoogleAuthenticationBotAccessors accessors, ILoggerFactory loggerFactory)
         {
-            if (conversationState == null)
-            {
-                throw new System.ArgumentNullException(nameof(conversationState));
-            }
+
+            _accessors = accessors ?? throw new ArgumentNullException(nameof(accessors));
+
+           
 
             if (loggerFactory == null)
             {
                 throw new System.ArgumentNullException(nameof(loggerFactory));
             }
 
-            _accessors = new GoogleAuthenticationBotAccessors(conversationState)
-            {
-                          };
-
             _logger = loggerFactory.CreateLogger<GoogleAuthenticationBot>();
             _logger.LogTrace("Turn start.");
+
+
+            // The DialogSet needs a DialogState accessor, it will call it when it has a turn context.
+            _dialogs = new DialogSet(accessors.ConversationDialogState);
+
+            // This array defines how the Waterfall will execute.
+            var waterfallSteps = new WaterfallStep[]
+            {
+               PromptStepAsync,
+               LoginStepAsync
+            };
+
+            // Add named dialogs to the DialogSet. These names are saved in the dialog state.
+            _dialogs.Add(new WaterfallDialog("details", waterfallSteps));
+            _dialogs.Add(new OAuthPrompt("auth", new OAuthPromptSettings { ConnectionName = "Google", Text = "Login Google", Title = "Login Google", Timeout = 300000 }, null));
         }
 
         /// <summary>
@@ -66,7 +79,55 @@ namespace GoogleAuthenticationBot
         /// <seealso cref="IMiddleware"/>
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
-          
+           
+
+
         }
+
+
+        /// <summary>
+        /// This <see cref="WaterfallStep"/> prompts the user to log in.
+        /// </summary>
+        /// <param name="step">A <see cref="WaterfallStepContext"/> provides context for the current waterfall step.</param>
+        /// <param name="cancellationToken" >(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> representing the operation result of the operation.</returns>
+        private static async Task<DialogTurnResult> PromptStepAsync(WaterfallStepContext step, CancellationToken cancellationToken)
+        {
+            return await step.BeginDialogAsync("auth", cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// In this step we check that a token was received and prompt the user as needed.
+        /// </summary>
+        /// <param name="step">A <see cref="WaterfallStepContext"/> provides context for the current waterfall step.</param>
+        /// <param name="cancellationToken" >(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> representing the operation result of the operation.</returns>
+        private static async Task<DialogTurnResult> LoginStepAsync(WaterfallStepContext step, CancellationToken cancellationToken)
+        {
+            // Get the token from the previous step. Note that we could also have gotten the
+            // token directly from the prompt itself. There is an example of this in the next method.
+            var tokenResponse = (TokenResponse)step.Result;
+            if (tokenResponse != null)
+            {
+                await step.Context.SendActivityAsync($"You are now logged in. Here is your token {tokenResponse.Token}", cancellationToken: cancellationToken);
+                return Dialog.EndOfTurn;
+            }
+
+            await step.Context.SendActivityAsync("Login was not successful please try again.", cancellationToken: cancellationToken);
+            return Dialog.EndOfTurn;
+        }
+
+
+        private static async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+
+            await turnContext.SendActivityAsync(
+        "Bienvenue. Ce bot permet de recueillir vos avis.",
+        cancellationToken: cancellationToken);
+
+        }
+
     }
 }
