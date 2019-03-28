188a8984c46d6fff88fecad68bc8028da95978e5
Hinault <ngdonfack@gmail.com>
2019-03-27 21:13:51 -0400

    Add service

---
+++ src/SentimentBot/TextAnalyticsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+
+namespace SentimentBot
+{
+    public class TextAnalyticsService
+    {
+
+        public ITextAnalyticsClient TextAnalytics;
+
+        public TextAnalyticsService()
+        {
+            TextAnalytics = new TextAnalyticsClient()
+        }
+
+    }
+}
